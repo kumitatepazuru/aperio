bfc3085e9b8b7bd2429355bd0439acb017ddfefc
kumitatepazuru <43287589+kumitatepazuru@users.noreply.github.com>
2026-08-05 22:22:31 +1000

    build: linuxでビルドできない問題を修正

--- src/gpu_util/src/texture_to_native/linux.rs
+++ src/gpu_util/src/texture_to_native/linux.rs
@@ -244,8 +244,12 @@ fn create_texture_from_dmabuf(
             raw_device_clone.free_memory(memory, None);
         });
 
-        let hal_texture =
-            device_guard.texture_from_raw(image, &hal_texture_desc, Some(drop_callback));
+        let hal_texture = device_guard.texture_from_raw(
+            image,
+            &hal_texture_desc,
+            Some(drop_callback),
+            wgpu::hal::vulkan::TextureMemory::External,
+        );
 
         // deviceガードをドロップしてからcreate_texture_from_halを呼ぶ
         drop(device_guard);
